e1f07e5bbbb2c80008ca65801ffe1c28ab379780
ddhananjay <dhananjayd510@gmail.com>
2024-11-06 09:55:28 -0600

    Update org.utils.DateUtils.groovy

--- src/org.utils.DateUtils.groovy
+++ src/org.utils.DateUtils.groovy
@@ -1,7 +1,12 @@
 package org.utils
 
-class DateUtils {
-    static String getTimestamp() {
+class HelperUtils implements Serializable {
+
+    static String getTimestamp() {  // Define as static to allow direct call
         return new Date().format("yyyy-MM-dd HH:mm:ss")
+    }
+
+    static String capitalizeString(String input) {
+        return input?.capitalize() ?: ""
     }
 }
