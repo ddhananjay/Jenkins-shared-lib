887d608b8aec106a50921c18790de3000d1763b7
ddhananjay <dhananjayd510@gmail.com>
2024-11-06 08:38:02 -0600

    Update uploadArtifactToDockerHub.groovy

--- vars/uploadArtifactToDockerHub.groovy
+++ vars/uploadArtifactToDockerHub.groovy
@@ -1,4 +1,4 @@
-def call(String imageName, String tag, String registry = 'docker.io', String dockerCredentialsId = 'dockerhub') {
+def call(String imageName, String tag, String username, String dockerCredentialsId = 'dockerhub') {
    echo 'Pushing docker file to docker hub..'
    withCredentials([usernamePassword(credentialsId: dockerCredentialsId) {
       docker.withRegistry('https://index.docker.io/v1/', dockerCredentialsId) {
