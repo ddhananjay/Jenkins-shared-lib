cbe3887ef9cba9df4e016f47e1080a6d843d6820
ddhananjay <dhananjayd510@gmail.com>
2024-11-06 10:00:57 -0600

    Update timestampedEcho.groovy

--- vars/timestampedEcho.groovy
+++ vars/timestampedEcho.groovy
@@ -1,6 +1,6 @@
 import org.utils.*
 def call(String message) {
-   // String timestamp = org.utils.DateUtils.getTimestamp()
-   // echo "[${timestamp}] ${message}"
+    String timestamp = org.utils.DateUtils.getTimestamp()
+    echo "[${timestamp}] ${message}"
     echo HelperUtils.capitalizeString('hieeee')
 }
