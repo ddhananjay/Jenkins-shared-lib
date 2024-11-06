d17c32c8c150f5eb9782e003e2449d654a65de98
ddhananjay <dhananjayd510@gmail.com>
2024-11-06 08:54:17 -0600

    Update uploadArtifactToDockerHub.groovy

--- vars/uploadArtifactToDockerHub.groovy
+++ vars/uploadArtifactToDockerHub.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String tag, String username, String dockerCredentialsId = 'dockerhub') {
    sh "echo Pushing docker file to docker hub..${username}"
      sh "echo Pushing docker file to docker hub..${imageName}"
-   withCredentials([usernamePassword(credentialsId: dockerCredentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD'))]) {
+   withCredentials([usernamePassword(credentialsId: dockerCredentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
        echo "Logging in to Docker Hub"
        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin ${registry}"
 
