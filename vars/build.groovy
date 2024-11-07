a7f6b394be62d77190f75b1a030d5608de98a8de
ddhananjay <dhananjayd510@gmail.com>
2024-11-06 22:44:33 -0600

    Create build.groovy

---
+++ vars/build.groovy
@@ -0,0 +1,14 @@
+def call(String language) {
+    if (language == "java") {
+        echo "Building a Java project"
+          sh './gradlew clean build'
+    } else if (language == "nodejs") {
+        echo "Building a Node.js project"
+        sh 'npm install && npm run build'
+    } else if (language == "python") {
+        echo "Building a Python project"
+        sh 'pip install -r requirements.txt && python setup.py install'
+    } else {
+        error "Unknown language: ${language}"
+    }
+}
