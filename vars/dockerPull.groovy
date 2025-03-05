ae3d589c4ff3e778c60495142721a4b32db59e5b
ddhananjay <dhananjayd510@gmail.com>
2025-03-05 11:37:43 -0600

    Create dockerPull.groovy

---
+++ vars/dockerPull.groovy
@@ -0,0 +1,14 @@
+def call(String imageName, String tag, String username, String dockerCredentialsId = 'dockerhub', String registry = 'docker.io') {
+   echo "Logging in to Docker Hub ..."
+   withCredentials([usernamePassword(credentialsId: dockerCredentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+       echo "Logging in to Docker Hub"
+       sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin ${registry}'
+
+      docker.withRegistry('https://index.docker.io/v1/', dockerCredentialsId) {
+                            sh """
+                                docker pull ${username}/${imageName}:${tag}
+                            """
+                  }
+       }
+
+}
