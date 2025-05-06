19b5acd80336b9365d9184f597dffbf29ba06976
ddhananjay <dhananjayd510@gmail.com>
2025-05-06 13:05:16 -0500

    Update deployToAKS.groovy

--- vars/deployToAKS.groovy
+++ vars/deployToAKS.groovy
@@ -1,9 +1,26 @@
-def call(String deploymentFile, String clusterName, String region) {
-    sh """
-        # Update kubeconfig to access EKS
-        aws eks update-kubeconfig --region ${region} --name ${clusterName}
+def call(String tenantId, String subscriptionId, String resourceGroup, String aksCluster, String dockerImage, ) {
+   stage('Azure Login') {
+        withCredentials([usernamePassword(credentialsId: azureCredsId, usernameVariable: 'AZ_CLIENT_ID', passwordVariable: 'AZ_CLIENT_SECRET')]) {
+            sh """
+                az login --service-principal -u $AZ_CLIENT_ID -p $AZ_CLIENT_SECRET --tenant ${tenantId}
+                az account set --subscription ${subscriptionId}
+            """
+        }
+    }
 
-        # Apply the Kubernetes deployment
-        kubectl apply -f ${deploymentFile}
-    """
+    stage('Get AKS Credentials') {
+        sh """
+            az aks get-credentials --resource-group ${resourceGroup} --name ${aksCluster} --overwrite-existing
+        """
+    }
+
+    stage('Deploy to AKS') {
+        def deploymentName = 'myapp-deployment'
+        def containerName = 'myapp-container'
+
+        sh """
+            kubectl set image deployment/${deploymentName} ${containerName}=${dockerImage} --record
+            kubectl rollout status deployment/${deploymentName}
+        """
+    }
 }
