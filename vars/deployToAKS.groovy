1f65964c612cb7d9ab35eb9e914d735d48b68d6f
ddhananjay <dhananjayd510@gmail.com>
2025-05-06 14:21:38 -0500

    Update deployToAKS.groovy

--- vars/deployToAKS.groovy
+++ vars/deployToAKS.groovy
@@ -1,4 +1,4 @@
-def call(String tenantId, String subscriptionId, String resourceGroup, String aksCluster, String dockerImage, String azureCredsId) {
+def call(String tenantId, String subscriptionId, String resourceGroup, String aksCluster, String dockerImage, String azureCredsId, String dockerImage) {
    stage('Azure Login') {
         withCredentials([usernamePassword(credentialsId: azureCredsId, usernameVariable: 'AZ_CLIENT_ID', passwordVariable: 'AZ_CLIENT_SECRET')]) {
             sh """
